658cfd7517c4ba2fe1cbe4800cc08caf623ba981
Stanislav Derkach <becks.ua@gmail.com>
2018-10-09 17:54:34 +0300

    create databases for servers, Pcs, Laptops

--- servers/salesite/models.py
+++ servers/salesite/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Servers(models.Model):
+    model_name = models.CharField(max_length=32)
+    quantity = models.IntegerField(default=0)
+    date = models.DateField("date arrives")
+    description = models.TextField(max_length=120)
+
+class PCs(models.Model):
+    model_name = models.CharField(max_length=32)
+    quantity = models.IntegerField(default=0)
+    date = models.DateField("date arrives")
+    description = models.TextField(max_length=120)
+
+class Laptops(models.Model):
+    model_name = models.CharField(max_length=32)
+    quantity = models.IntegerField(default=0)
+    date = models.DateField("date arrives")
+    description = models.TextField(max_length=120)
