734b7a1d294dff444564fae615c268e803cfd145
Stanislav Derkach <becks.ua@gmail.com>
2018-10-10 17:26:53 +0300

    modification2 db

--- servers/salesite/models.py
+++ servers/salesite/models.py
@@ -1,27 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Customers(models.Model):
+class Customer(models.Model):
     customer = models.CharField(max_length=32)
     contact = models.CharField(max_length=32)
     phone = models.CharField(max_length=32)
     email = models.EmailField()
 
-class Servers(models.Model):
+class Server(models.Model):
     customer = models.ForeignKey(Customers)
     model_name = models.CharField(max_length=32)
     quantity = models.IntegerField(default=0)
     date = models.DateField("date arrives")
     description = models.TextField(max_length=120)
 
-class PCs(models.Model):
+class PC(models.Model):
     customer = models.ForeignKey(Customers)
     model_name = models.CharField(max_length=32)
     quantity = models.IntegerField(default=0)
     date = models.DateField("date arrives")
     description = models.TextField(max_length=120)
 
-class Laptops(models.Model):
+class Laptop(models.Model):
     customer = models.ForeignKey(Customers)
     model_name = models.CharField(max_length=32)
     quantity = models.IntegerField(default=0)
