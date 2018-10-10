ca09f18630c55a192689160df2658a1fbe26a3d9
Stanislav Derkach <becks.ua@gmail.com>
2018-10-10 17:24:39 +0300

    modification db

--- servers/salesite/models.py
+++ servers/salesite/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Customers(models.Model):
+    customer = models.CharField(max_length=32)
+    contact = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32)
+    email = models.EmailField()
+
 class Servers(models.Model):
+    customer = models.ForeignKey(Customers)
     model_name = models.CharField(max_length=32)
     quantity = models.IntegerField(default=0)
     date = models.DateField("date arrives")
     description = models.TextField(max_length=120)
 
 class PCs(models.Model):
+    customer = models.ForeignKey(Customers)
     model_name = models.CharField(max_length=32)
     quantity = models.IntegerField(default=0)
     date = models.DateField("date arrives")
     description = models.TextField(max_length=120)
 
 class Laptops(models.Model):
+    customer = models.ForeignKey(Customers)
     model_name = models.CharField(max_length=32)
     quantity = models.IntegerField(default=0)
     date = models.DateField("date arrives")
     description = models.TextField(max_length=120)
+
+
--- servers/servers/settings.py
+++ servers/servers/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bootstrap4',
     'fa',
-    'salesite',
+    'salesite.apps.SalesiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
