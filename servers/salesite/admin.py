08d7642b02e805a43fa8bcfca74cad0bc144c80b
Stanislav Derkach <becks.ua@gmail.com>
2018-10-11 17:38:21 +0300

    add model into admin

--- servers/salesite/admin.py
+++ servers/salesite/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from salesite.models import Customer, Order, Server, PC, Laptop
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Server)
+admin.site.register(PC)
+admin.site.register(Laptop)
+admin.site.register(Order)
+
+
+
