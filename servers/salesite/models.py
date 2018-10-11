c42864356b577e878b1f4022a52481351e30ff86
Stanislav Derkach <becks.ua@gmail.com>
2018-10-11 17:13:19 +0300

    add new columns into db

--- servers/salesite/models.py
+++ servers/salesite/models.py
@@ -7,10 +7,15 @@ class Customer(models.Model):
     contact = models.CharField(max_length=32)
     phone = models.CharField(max_length=32)
     email = models.EmailField()
+    def __str__(self):
+        return ("{}".format(self.customer))
 
 class Order(models.Model):
     date = models.DateField(time.asctime(time.localtime(time.time())))
     customer = models.ForeignKey(Customer, on_delete= models.PROTECT)
+
+    def __str__(self):
+        return ("{}".format(self.id + " " + self.customer))
 
 
 class Server(models.Model):
@@ -18,7 +23,11 @@ class Server(models.Model):
     model_name = models.CharField(max_length=32)
     quantity = models.IntegerField(default=0)
     date = models.DateField("date arrives")
-    description = models.TextField(max_length=120)
+    description = models.TextField(max_length=120, null=True)
+
+    def __str__(self):
+        return ("{}".format(self.model_name))
+
 
 class PC(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
