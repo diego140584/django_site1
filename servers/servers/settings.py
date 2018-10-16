cbf49c2f1f88de242efe5f2c74096dfccf5db022
Stanislav Derkach <becks.ua@gmail.com>
2018-10-16 14:54:01 +0300

    changes for carousel in index.html

--- servers/servers/settings.py
+++ servers/servers/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'salesite.apps.SalesiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'salesite.apps.SalesiteConfig'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
