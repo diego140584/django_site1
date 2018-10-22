54bec0e812c1362f009bd3318b6ce22e3b1646b9
Stanislav Derkach <becks.ua@gmail.com>
2018-10-22 13:42:17 +0300

    change login/authorization form

--- servers/salesite/forms.py
+++ servers/salesite/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 
 
+
 class LoginForm(forms.Form):
-    name = forms.CharField()
-    lastName = forms.CharField()
-    login = forms.CharField()
-    email = forms.CharField()
-    phone = forms.CharField()
+
+    login = forms.CharField(label="login", help_text="Введите свое имя")
+    password = forms.CharField(widget=forms.PasswordInput, help_text="Введите свое пароль")
