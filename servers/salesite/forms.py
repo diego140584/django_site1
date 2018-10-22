8972aac0a1e92a4b573f780bc82338e6c7fa1709
Stanislav Derkach <becks.ua@gmail.com>
2018-10-22 16:04:35 +0300

    changes in logform

--- servers/salesite/forms.py
+++ servers/salesite/forms.py
@@ -4,5 +4,5 @@ from django import forms
 
 class LoginForm(forms.Form):
 
-    login = forms.CharField(label="login", help_text="Введите свое имя")
-    password = forms.CharField(widget=forms.PasswordInput, help_text="Введите свое пароль")
+    login = forms.CharField(label="login", help_text="Введите свое имя", required=True)
+    password = forms.CharField(widget=forms.PasswordInput, help_text="Введите свое пароль", required=True, min_length=6)
--- servers/salesite/views.py
+++ servers/salesite/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.http import HttpResponse,request
 
 
 # Create your views here.
 def index(request):
-    logform = LoginForm()
-    context = {"form": logform}
+    if request.method == "POST":
+        lf = LoginForm(request.POST)
+        if lf.is_valid():
+            login = lf.cleaned_data("login")
+            return HttpResponse("<h1>Hello {}!</h1>".format(login))
 
-    return render(request, 'index.html', context)
+        else:
+            return HttpResponse("<h1>Invalid data!</h1>")
+    else:
+        logform = LoginForm()
+        context = {"form": logform}
+        return render(request, 'index.html', context)
+
 
 
