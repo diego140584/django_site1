bb6f1b83ddc95a5cc8bba147c2d773993f4d2f5b
Stanislav Derkach <becks.ua@gmail.com>
2018-12-12 16:56:56 +0200

    create search for laptop

--- servers/salesite/views.py
+++ servers/salesite/views.py
@@ -6,6 +6,8 @@ from django.views.generic import View
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import Http404
 
 
 # Create your views here.
@@ -24,21 +26,19 @@ class Index(View):
             return HttpResponse("<h1>Invalid data!</h1>")
 
 
-
-
 class RegForm(View):
 
-   def get(self, request):
+    def get(self, request):
         regForm = RForm()
         return render(request, 'modalRegister.html', context={'regForm': regForm})
 
-   def post(self, request):
-       regF = RForm(request.POST)
-       if regF.is_valid():
-           regF.save()
-           return redirect(regF)
-       else:
-           return render(request, 'modalRegister.html', context={'regForm': regF})
+    def post(self, request):
+        regF = RForm(request.POST)
+        if regF.is_valid():
+            regF.save()
+            return redirect(regF)
+        else:
+            return render(request, 'modalRegister.html', context={'regForm': regF})
 
 
 class PricesTemplate(View):
@@ -51,12 +51,17 @@ class PricesLaptop(View):
 
     def get(self, request):
 
-        laptop = Laptop.objects.all()
+        search_query = self.request.GET.get('search', default='')
+        if search_query:
+            laptop = Laptop.objects.filter(name__icontains=search_query)
+        else:
+            laptop = Laptop.objects.all()
+
         paginator = Paginator(laptop, 3)
         page_number = self.request.GET.get('current_page', 1)
         current_page = paginator.get_page(page_number)
         has_pages = current_page.has_other_pages()
-        search_query = self.request.GET.get('search', '')
+
         if current_page.has_previous():
             prew_page = '?current_page={}'.format(current_page.previous_page_number())
         else:
@@ -67,19 +72,17 @@ class PricesLaptop(View):
         else:
             next_page = ''
         count = paginator.page_range.__len__()
-        print('count-', count)
+
         pages = {
             'pg': paginator,
             'page': current_page,
             'has': has_pages,
             'preview': prew_page,
             'next': next_page,
-            'count':count,
+            'count': count,
         }
-
-
-
         return render(request, "prices_devices.html", context=pages)
+
 
 class PricesPC(View):
     def get(self, request):
@@ -99,18 +102,11 @@ class PricesServer(View):
         return render(request, 'prices_devices.html', context={'devices': page})
 
 
-
 class Fedback(View):
-
     def get(self, request):
         return render(request, "fedback.html")
 
 
-
 class ShowDew(View):
-
     def get(self, request):
         return HttpResponse(request, "<h1>It works!")
-
-
-
